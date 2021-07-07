1ea05778d18b7ad34835f8a4f0bf7bf178b15bdb
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-07 14:16:32 +0530

    new program UPR : master

---
+++ OponNum.java
@@ -0,0 +1,11 @@
+public class OponNum {
+    int a,b;
+    public static void main(String[] args) {
+        OponNum num = new OponNum();
+        num.a = 10;
+        num.b = 20;
+
+        System.out.println("Sum is " + (num.a + num.b));
+        
+    }
+}

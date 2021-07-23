abd1becb485a83310bde5c38fea665b9405eb3be
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-23 14:51:02 +0530

    testing : master

--- MethOver.java
+++ MethOver.java
@@ -7,7 +7,7 @@
 public class MethOver {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(Adder.Add(11, 23));
+        System.out.println(Adder.Add(11, 23.5));
         System.out.println(Adder.Add(23, 48));
     }
     public static void main(String args) {
@@ -16,11 +16,11 @@ public class MethOver {
 
 }
 class Adder {
-    public static double Add(double num1, double num2) {
+    public static int Add(int num1, int num2) {
         return (num1 + num2);
     }
 
-    public static int Add(int num1, int num2) {
+    public static double Add(int num1, int num2) {
         return (num1 + num2);
     }
 }

733559cf093049f7f897eb2419dd1d116467cbe0
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-23 15:13:57 +0530

    no error : master

--- MethOver.java
+++ MethOver.java
@@ -6,17 +6,20 @@
  */
 public class MethOver {
     public static void main(String[] args) {
-        Adder adder = new Adder();
-        System.out.println(Adder.Add(11, 23.5));
-        System.out.println(Adder.Add(23, 48));
+        Arith adder = new Arith();
+        System.out.println(Adder.Add(11, 23) + " 1 ");
+        System.out.println(Adder.Add(23, 48) + " 2 ");
     }
 }
+
 class Adder {
-    public static float Add(int num1, int num2) {
+    public static int Add(int num1, int num2) {
         return (num1 + num2);
     }
+}
 
-    public static double Add(double num1, double num2) {
+class Arith extends Adder {
+    public static int Add(int num1, int num2) {
         return (num1 + num2);
     }
 }

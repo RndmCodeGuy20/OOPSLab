10b6a81dc59e586458492e743df3792b787c054c
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-28 13:55:08 +0530

    static final variable : master

---
+++ statfin.java
@@ -0,0 +1,14 @@
+public class statfin {
+    
+    static final int data;
+
+    statfin(int val){
+        data = val;
+    }
+    public static void main(String[] args) {
+        
+        statfin objStatfin = new statfin(30);
+        
+        System.out.println(objStatfin.data);
+    }
+}

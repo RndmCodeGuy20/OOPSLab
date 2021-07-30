30635c3cbcd38074b7d0f3d36de26bfb9bd9f719
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-30 20:46:45 +0530

    all funcs UPR : master

--- MainBank.java
+++ MainBank.java
@@ -14,68 +14,120 @@ public class MainBank {
 
             Savings accSavings = new Savings();
 
-            System.out.print("Enter your credentials : ");
-            
-            System.out.print("Enter Name :");
+            System.out.print("Enter your credentials : \n\n");
+
+            System.out.print("Enter Name : ");
             String Name = scan.nextLine();
 
-            System.out.print("Enter Age : ");
+            System.out.print("\nEnter Age : ");
             int Age = scan.nextInt();
 
-            System.out.print("Enter Gender : ");
+            System.out.print("\nEnter Gender : ");
             char Gend = scan.next().charAt(0);
 
-            if(Name.equals("Shantanu Mane")){
+            if (Name.equals("Shantanu Mane")) {
                 InitialBal = 2000;
-            }
-            else if(Name.equals("Vaishali Mane")){
+            } else if (Name.equals("Vaishali Mane")) {
                 InitialBal = 900;
             }
 
             accSavings.InfoUserSavings(Name, Age, Gend, InitialBal);
-            
-            System.out.println("\n\n What operation do you want to do?" + "\n\n" + "Press A to open new account" + "\n\n" + "Press B to deposit amount " + "\n\n" + "Press C to debit amount" + "\n\n" + "Press D to close account");
+
+            System.out.println("\n\nWhat operation do you want to do?" + "\n\n" + "Press A to open new account" + "\n\n"
+                    + "Press B to deposit amount " + "\n\n" + "Press C to debit amount" + "\n\n"
+                    + "Press D to close account");
             char OperChoice;
 
             OperChoice = scan.next().charAt(0);
 
             switch (OperChoice) {
                 case 'A':
-                System.out.println("How much amount do you want to deposit?");
-                int inpAmo = scan.nextInt();
-                accSavings.OpenAccSav(Name, Age, Gend, inpAmo);
+                    System.out.println("How much amount do you want to deposit?");
+                    int inpAmo = scan.nextInt();
+                    accSavings.OpenAccSav(Name, Age, Gend, inpAmo);
                     break;
                 case 'B':
-                System.out.println("How much amount do you want to deposit?");
-                inpAmo = scan.nextInt();
-                accSavings.CreditSave(InitialBal ,inpAmo);
+                    System.out.println("How much amount do you want to deposit?");
+                    inpAmo = scan.nextInt();
+                    accSavings.CreditSave(InitialBal, inpAmo);
                     break;
 
                 case 'C':
-                System.out.println("How much amount do you want to debit?");
-                int debit = scan.nextInt();
-                accSavings.WithdrawSav(InitialBal,debit);
+                    System.out.println("How much amount do you want to debit?");
+                    int debit = scan.nextInt();
+                    accSavings.WithdrawSav(InitialBal, debit);
                     break;
 
                 case 'D':
-                System.out.println("Confirm Delete Account?");
-                String delchoice = scan.nextLine();
-                if (delchoice.equals("Yes")) {
-                    accSavings.DeleteAccSave();
-                }
+                    System.out.println("Confirm Delete Account?");
+                    String delchoice = scan.nextLine();
+                    accSavings.DeleteAccSave(delchoice);
+
                 default:
                     break;
             }
 
-        } else if (choice.equals("Credit")) {
+        } else if (choice.equals("Currents")) {
+            Current accCurrents = new Current();
 
+            System.out.print("Enter your credentials : \n\n");
+
+            System.out.print("Enter Name :");
+            String Name = scan.nextLine();
+
+            System.out.print("\nEnter Age : ");
+            int Age = scan.nextInt();
+
+            System.out.print("\nEnter Gender : ");
+            char Gend = scan.next().charAt(0);
+
+            if (Name.equals("Shantanu Mane")) {
+                InitialBal = 9000;
+            } else if (Name.equals("Vaishali Mane")) {
+                InitialBal = 900;
+            }
+
+            accCurrents.InfoUserCurrent(Name, Age, Gend, InitialBal);
+
+            System.out.println("\n\nWhat operation do you want to do?" + "\n\n" + "Press A to open new account" + "\n\n"
+                    + "Press B to deposit amount " + "\n\n" + "Press C to debit amount" + "\n\n"
+                    + "Press D to close account");
+            char OperChoice;
+
+            OperChoice = scan.next().charAt(0);
+
+            switch (OperChoice) {
+                case 'A':
+                    System.out.println("How much amount do you want to deposit?");
+                    int inpAmo = scan.nextInt();
+                    accCurrents.OpenAccCurr(Name, Age, Gend, inpAmo);
+                    break;
+                case 'B':
+                    System.out.println("How much amount do you want to deposit?");
+                    inpAmo = scan.nextInt();
+                    accCurrents.CreditCurr(InitialBal, inpAmo);
+                    break;
+
+                case 'C':
+                    System.out.println("How much amount do you want to debit?");
+                    int debit = scan.nextInt();
+                    accCurrents.WithdrawCurr(InitialBal, debit);
+                    break;
+
+                case 'D':
+                    System.out.println("Confirm Delete Account?");
+                    String delchoice = scan.nextLine();
+                    accCurrents.DeleteAccCurr(delchoice);
+                default:
+                    System.out.println("Wrong CHOICE!!!");
+                    break;
+            }
         } else {
             System.out.println("Wrong choice!!!");
         }
         scan.close();
     }
 }
-
 
 //=====//====//=====//
 
@@ -84,22 +136,24 @@ class Savings extends MainBank {
     final int MinBalSav = 1000;
 
     public void OpenAccSav(String Name, int Age, char Gend, int inpAmount) {
-        int min = 70000;  
-        int max = 80000; 
-        
-        int b = (int)(Math.random()*(max-min+1)+min);
-        
+        int min = 70000;
+        int max = 80000;
+
+        int b = (int) (Math.random() * (max - min + 1) + min);
+
         if (inpAmount < MinBalSav) {
             System.out.println("Minimum balance not satisfied, CANNOT open account!!! Deposit amount more than 1000!");
         } else {
             TotalBalSav = inpAmount;
-            System.out.println("Account created successfully!!!" + "\n\n" + "Your Unique ID is " + b + "\n\n" + "Total Balance is : " + TotalBalSav);
+            System.out.println("Account created successfully!!!" + "\n\n" + "Your Unique ID is " + b + "\n\n"
+                    + "Total Balance is : " + TotalBalSav);
         }
     }
 
     public void InfoUserSavings(String Name, int Age, char Gend, int inpBal) {
 
-        System.out.println("User Credentials : " + Name + " " + Age + " " + Gend + "Input Amount " + inpBal);
+        System.out.println(
+                "\n\nUser Credentials : \t" + "\nName : " + Name + " \nAge : \t" + Age + " \nGender : \t" + Gend + "\nCurrent Balance Amount : \t" + inpBal);
         if (inpBal < MinBalSav) {
             System.out.println("Minimum Balance not satisfied!");
         }
@@ -128,18 +182,67 @@ class Savings extends MainBank {
 
     }
 
-    public void DeleteAccSave() {
+    public void DeleteAccSave(String choiceString) {
         System.out.println("Account Closed successfully!!!");
     }
 }
 
-//====//====//=====//
+// ====//====//=====//
 
-// class Current extends MainBank {
-// int TotalBalCurr;
+class Current extends MainBank {
+    int TotalBalCurr;
 
-// }
+    final int MinBalCurr = 5000;
 
-// class Customer {
+    public void OpenAccCurr(String Name, int Age, char Gend, int inpAmount) {
+        int min = 70000;
+        int max = 80000;
 
-// }
+        int b = (int) (Math.random() * (max - min + 1) + min);
+
+        if (inpAmount < MinBalCurr) {
+            System.out.println("Minimum balance not satisfied, CANNOT open account!!! Deposit amount more than 1000!");
+        } else {
+            TotalBalCurr = inpAmount;
+            System.out.println("Account created successfully!!!" + "\n\n" + "Your Unique ID is " + b + "\n\n"
+                    + "Total Balance is : " + TotalBalCurr);
+        }
+    }
+
+    public void InfoUserCurrent(String Name, int Age, char Gend, int inpBal) {
+
+        System.out.println("\n\nUser Credentials : \t" + "\nName : " + Name + " \nAge : \t" + Age + " \nGender : \t" + Gend
+                + "\nCurrent Balance Amount : \t" + inpBal);
+        if (inpBal < MinBalCurr) {
+            System.out.println("Minimum Balance not satisfied!");
+        }
+        TotalBalCurr = inpBal;
+    }
+
+    public void WithdrawCurr(int InitialBal, int amWithCurr) {
+        TotalBalCurr = InitialBal;
+
+        System.out.println("Your Total Balance is " + TotalBalCurr);
+        if (amWithCurr < TotalBalCurr) {
+            TotalBalCurr = TotalBalCurr - amWithCurr;
+        } else {
+            System.out.println("Insufficient Balance!!!");
+        }
+        System.out.println("Remaining amount in account after withdrawal is " + TotalBalCurr);
+    }
+
+    public void CreditCurr(int InitialBal, int CredAmCurr) {
+        TotalBalCurr = InitialBal;
+        System.out.println("Your Total Balance is " + TotalBalCurr);
+
+        TotalBalCurr = TotalBalCurr + CredAmCurr;
+
+        System.out.println("Remaining amount in account after credit is " + TotalBalCurr);
+
+    }
+
+    public void DeleteAccCurr(String choiceString) {
+        System.out.println("Account Closed successfully!!!");
+    }
+
+}

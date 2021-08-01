d4cc14fb7511fc1e5b2a49783278dad64b9beaec
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-01 21:22:23 +0530

    Code reused for assignment : master

--- Student.java
+++ Student.java
@@ -2,12 +2,16 @@ public class Student {
 
     String Name;
     int Roll;
+    double Marks;
     public static void main(String[] args) {
-        Student info = new Student();
-        info.Name = "John";
-        info.Roll = 2;
+       MyRecord studObj = new MyRecord();
+       studObj.display("Shantanu Mane", 85, 98.05);     
 
-        System.out.println("Name is " + info.Name + "\n" + "Roll no. is " + info.Roll);
+    }
+}
 
+class MyRecord extends Student{
+    public void display(String Name, int Roll, double Marks) {
+        System.out.println("Name of student : " + Name + "\n\n" + "Roll Number of Student : " + Roll + "\n\n" + "Marks obtained by the student : " + Marks + "\n\n");
     }
 }

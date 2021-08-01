717889f4fc37dec3c033eec9cb7c4dfd83bd13d8
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-01 16:43:18 +0530

    Subfolder Compilation : master

---
+++ InnerFolder/Simple.java
@@ -0,0 +1,20 @@
+package InnerFolder;
+
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    /**
+     * *1. Create a package (subfolder).
+     * 
+     * *2. Compile java file situated in the package using ->
+     * ? javac Filename.java
+     * 
+     * *3. Compile the whole package using ->
+     * ? javac -d . filename.java
+     * 
+     * *4. To run the file in the package write ->
+     * ? java Packagename.filename
+     * 
+     */
+}

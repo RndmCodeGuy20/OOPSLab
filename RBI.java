513aa58d40a6afe40dea920efe15128c37c0ccfb
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-02 16:12:59 +0530

    Rate of Int UPR : master

---
+++ RBI.java
@@ -0,0 +1,33 @@
+public class RBI{
+    public int getRateOfInterest(){
+        return 0;
+    }
+    public static void main(String[] args) {
+        SBI S = new SBI();
+        ICICI I = new ICICI();
+        Axis A = new Axis();
+
+        System.out.println("Rate of Interest at SBI : " + S.getRateOfInterest());
+        System.out.println("Rate of Interest at ICICI : " + I.getRateOfInterest());
+        System.out.println("Rate of Interest at Axis : " + A.getRateOfInterest());
+
+    }
+}
+
+class SBI extends RBI{
+    public int getRateOfInterest() {
+        return 8;
+    }
+}
+
+class ICICI extends RBI {
+    public int getRateOfInterest() {
+        return 9;
+    }
+}
+
+class Axis extends RBI{
+    public int getRateOfInterest() {
+        return 10;
+    }
+}

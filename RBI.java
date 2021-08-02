9d83cb91d73d0bdc66c0351690df17bb57db77ec
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-02 19:55:44 +0530

    code optimised : master

--- RBI.java
+++ RBI.java
@@ -1,20 +1,23 @@
-public class RBI{
-    public int getRateOfInterest(){
+public class RBI {
+    public int getRateOfInterest() {
         return 0;
     }
+
     public static void main(String[] args) {
         SBI S = new SBI();
         ICICI I = new ICICI();
         Axis A = new Axis();
 
         System.out.println("Rate of Interest at SBI : " + S.getRateOfInterest());
+
         System.out.println("Rate of Interest at ICICI : " + I.getRateOfInterest());
+
         System.out.println("Rate of Interest at Axis : " + A.getRateOfInterest());
 
     }
 }
 
-class SBI extends RBI{
+class SBI extends RBI {
     @Override
     public int getRateOfInterest() {
         return 8;
@@ -28,7 +31,7 @@ class ICICI extends RBI {
     }
 }
 
-class Axis extends RBI{
+class Axis extends RBI {
     @Override
     public int getRateOfInterest() {
         return 10;

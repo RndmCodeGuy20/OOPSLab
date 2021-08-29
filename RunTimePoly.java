c747742ae6082405b6f4f81e3f55548178877a36
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-29 23:21:33 +0530

    comments added : master

--- RunTimePoly.java
+++ RunTimePoly.java
@@ -1,3 +1,11 @@
+/**
+ * @author Shantanu Mane
+ * @version 20.20
+ * @brief Demonstrate use of run time polymorphism in the case of multilevel
+ *        inheritance.
+ * @RndmCodeGuy20
+ */
+
 public class RunTimePoly {
     public static void main(String[] args) {
         OS os = new OS("Operating Software");
@@ -54,3 +62,11 @@ class WinMob extends Win {
         System.out.println("Feature of " + Sys);
     }
 }
+
+// * OUTPUT
+// PS C:\OOPSLab> javac .\RunTimePoly.java 
+// PS C:\OOPSLab> java RunTimePoly
+// Feature of Operating Software
+// Feature of DOS
+// Feature of Windows OS
+// Feature of Windows OS for Mobile
